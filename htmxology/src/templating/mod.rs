6ecb7202233c755dc3be9ebfec642e449b38206f
Julien Kauffmann <julienkauffmann85+github@gmail.com>
2025-05-15 06:39:32 -0400

    Exposed the values from Askama

--- htmxology/src/templating/mod.rs
+++ htmxology/src/templating/mod.rs
@@ -1,18 +1,29 @@
 //! Templating facilities.
 
 /// Render a template into an Axum response.
-pub trait RenderIntoResponse {
+pub trait RenderIntoResponse: Sized {
     /// Render the template into a response.
-    fn render_into_response(self) -> axum::response::Response;
+    fn render_into_response(self) -> axum::response::Response {
+        self.render_into_response_with_values(&())
+    }
+
+    /// Render the template into a response, with values.
+    fn render_into_response_with_values(
+        self,
+        values: &dyn askama::Values,
+    ) -> axum::response::Response;
 }
 
 #[cfg(feature = "templating")]
 impl<T: askama::Template> RenderIntoResponse for T {
-    fn render_into_response(self) -> axum::response::Response {
+    fn render_into_response_with_values(
+        self,
+        values: &dyn askama::Values,
+    ) -> axum::response::Response {
         use axum::response::IntoResponse;
         use tracing::error;
 
-        match self.render() {
+        match self.render_with_values(values) {
             Ok(body) => {
                 let mut headers = http::HeaderMap::new();
                 headers.insert(
